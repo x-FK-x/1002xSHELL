97e8a259dc62379ef204dc3bd8130449842154de
x-FK-x <159540593+x-FK-x@users.noreply.github.com>
2025-06-26 14:00:34 +0200

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-TARGET_DIR="/XDOSshell"
+TARGET_DIR="/1002xSHELL"
 
-echo "🗑️ Removing XDOSshell directory at $TARGET_DIR ..."
+echo "🗑️ Removing 1002xSHELL directory at $TARGET_DIR ..."
 rm -rf "$TARGET_DIR"
 
 echo "🧹 Removing aliases from /etc/bash.bashrc ..."
-sed -i '/alias XDOSshell=/d' /etc/bash.bashrc
+sed -i '/alias 1002xSHELL=/d' /etc/bash.bashrc
 sed -i '/alias cmd=/d' /etc/bash.bashrc
 source /etc/bash.bashrc
 
 
-echo "🗑️ Removing desktop entry /usr/share/applications/xdosshell.desktop ..."
-rm -f /usr/share/applications/xdosshell.desktop
+echo "🗑️ Removing desktop entry /usr/share/applications/1002xSHELL.desktop ..."
+rm -f /usr/share/applications/1002xSHELL.desktop
 
-echo "✅ XDOSshell has been uninstalled successfully."
+echo "✅ 1002xSHELL has been uninstalled successfully."
